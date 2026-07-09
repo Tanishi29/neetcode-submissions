286bf2fad74ceb37f45a172282e27ca31413839b
Tanishi Kumar <tanishikumar12@gmail.com>
2026-07-09 15:38:46 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+
+        int max = 0;
+        int profit = 0;
+        int lowestBuy = prices[0];
+
+        for(int i = 0; i < prices.length; i++){
+
+            if(prices[i] < lowestBuy){
+                lowestBuy = prices[i];
+            }
+
+            profit = prices[i] - lowestBuy;
+
+            if(profit > max){
+                max = profit;
+            }
+            
+        }
+
+        return max;
+        
+    }
+}
