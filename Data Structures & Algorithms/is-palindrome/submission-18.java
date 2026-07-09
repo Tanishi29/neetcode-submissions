2e07c61022fcdc76f2cf46ba09ff8aa9d4015684
Tanishi Kumar <tanishikumar12@gmail.com>
2026-07-09 11:02:07 -0700

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        String noSpace = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        char [] arr = noSpace.toCharArray();
+        boolean isPalindrome = true;
+
+        int left = 0; 
+        int right = arr.length - 1;
+
+        
+            while(left <= right){
+                if(arr[left] != arr[right]){
+                
+                isPalindrome =  false;
+                
+            }
+            left++;
+            right--;
+            }
+            
+
+            
+
+        return isPalindrome;
+        
+        
+    }
+}
