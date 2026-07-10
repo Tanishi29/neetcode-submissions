dfbcd1ea860de42a6c63a457ee1069da76441378
Tanishi Kumar <tanishikumar12@gmail.com>
2026-07-10 14:26:16 -0700

    Add: daily-temperatures - submission-18

---
+++ Data Structures & Algorithms/daily-temperatures/submission-18.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        /*Stack<Integer> stack = new Stack<>();
+        int [] arr = new int [temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++){
+            
+
+            while((!stack.empty()) && temperatures[i] > temperatures[stack.peek()]){
+                int prevIndex = stack.pop();
+                arr[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return arr;*/
+
+        int [] arr = new int [temperatures.length];
+        
+        for(int i = 0; i < temperatures.length; i++){
+            for(int j = i; j < temperatures.length; j++){
+                if(temperatures[j] > temperatures[i] && i != j){
+                    int diff = j - i;
+                    arr[i] = diff;
+                    break;
+                }
+                
+            }
+        }
+
+        return arr;
+        
+    }
+}
