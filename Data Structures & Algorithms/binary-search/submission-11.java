17706e6c8c832f63ae79f58907a085e58780a071
Tanishi Kumar <tanishikumar12@gmail.com>
2026-07-09 22:34:32 -0700

    Add: binary-search - submission-11

---
+++ Data Structures & Algorithms/binary-search/submission-11.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int low = 0; 
+        int high = nums.length - 1;
+        
+
+        while(low <= high){
+            int mid = (high + low) / 2; //some basic avg/ mid 
+
+        if(nums[mid] == target){
+            return mid;
+        }
+        else if(nums[mid] < target){
+            low = mid + 1;  //like 2 pointers, move the smaller side to increment when result < target
+        }
+        else{
+                high = mid - 1; //like 2 ointers, move the right side to decrement when result > target
+        }
+        }
+
+        return -1;
+        
+    }
+}
